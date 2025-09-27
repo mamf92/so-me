15f011a2baee4d5822eaa0f3338ddf9cb03a10ae
Martin Fischer <115947567+mamf92@users.noreply.github.com>
2025-09-27 23:51:20 +0200

    Modified the vite.config.ts to have a different base when in production. Still works on dev. Also created a docs/ folder to serv build to Github Pages.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({
-  base: 'so-me',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/so-me/' : '/',
+  build: { outDir: 'docs' },
   plugins: [tailwindcss()],
-});
+}));
